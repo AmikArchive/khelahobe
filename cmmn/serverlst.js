e31585f3445abccf97ebacd864382eea8d9e4257
Your Name <you@example.com>
2022-11-29 00:19:50 +0600

    3rd this day

--- cmmn/serverlst.js
+++ cmmn/serverlst.js
@@ -1,10 +1,20 @@
 document.getElementById("serverlst").innerHTML =
 `
-<ul>
-  <li><a href="server0.html">Server-0</a></li>
+<ul class="srvrlst">
   <li><a href="server1.html">Server-1</a></li>
   <li><a href="server2.html">Server-2</a></li>
   <li><a href="server3.html">Server-3</a></li>
+  <li><a href="server4.html">Server-4</a></li>
+  <li><a href="server5.html">Server-5</a></li>
+</ul>
+
+`;
+
+document.getElementById("proserverlst").innerHTML =
+`
+<ul class="srvrlst">
+  <li><a href="#">Pro_Server-1</a></li>
+  <li><a href="#">Pro_Server-2</a></li>
 </ul>
 
 `;
