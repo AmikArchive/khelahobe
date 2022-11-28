3b2eb25a4c391bf2d7cd138484a4f7968cda37f1
AmikArchive <112723934+AmikArchive@users.noreply.github.com>
2022-11-28 22:56:33 +0600

    Update serverlst.js

--- cmmn/serverlst.js
+++ cmmn/serverlst.js
@@ -1,7 +1,7 @@
 document.getElementById("serverlst").innerHTML =
 `
 <ul>
-  <li><a href="server0.html">Server-1</a></li>
+  <li><a href="server0.html">Server-0</a></li>
   <li><a href="server1.html">Server-1</a></li>
   <li><a href="server2.html">Server-2</a></li>
   <li><a href="server3.html">Server-3</a></li>
