4a68a5d3634932aa061a4399d99bd909c9771398
Your Name <you@example.com>
2022-11-29 15:56:25 +0600

    ready

--- cmmn/serverlst.js
+++ cmmn/serverlst.js
@@ -1,13 +1,12 @@
 document.getElementById("serverlst").innerHTML =
 `
 <ul class="srvrlst">
-  <li><a href="server1.html">Server-1</a></li>
-  <li><a href="server2.html">Server-2</a></li>
-  <li><a href="server3.html">Server-3</a></li>
-  <li><a href="server4.html">Server-4</a></li>
-  <li><a href="server5.html">Server-5</a></li>
-  <li><a href="server6.html">Server-6</a></li>
-  <li><a href="server7.html">Server-7</a></li>
+  <li><a href="./server.html?url=https://talgach.xyz/bdix14Join:@fifaxlive.m3u8">Server-1</a></li>
+  <li><a href="./server.html?url=https://talgach.xyz/FWC2-Join:@fifaxlive.m3u8">Server-2</a></li>
+  <li><a href="./server.html?url=https://talgach.xyz/FWC4-Join:@fifaxlive.m3u8">Server-3</a></li>
+  <li><a href="./server.html?url=https://talgach.xyz/FWC-Join:@fifaxlive.m3u8">Server-4</a></li>
+  <li><a href="./server.html?url=https://talgach.xyz/FWC5-Join:@fifaxlive.m3u8">Server-5</a></li>
+  <li><a href="./server.html?url=https://talgach.xyz/sntsd-join:@fifaxlive.m3u8">Server-6</a></li>
 </ul>
 
 `;
@@ -15,8 +14,16 @@ document.getElementById("serverlst").innerHTML =
 document.getElementById("proserverlst").innerHTML =
 `
 <ul class="srvrlst">
-  <li><a href="proserver1.html">Pro_Server-1</a></li>
-  <li><a href="#">Pro_Server-2</a></li>
+  <li><a href="./server.html?url=http://103.102.27.173/live/tvfeed1001/index.m3u8">ProServer-1</a></li>
+  <li><a href="./server.html?url=http://tv2.efiberbd.net:8080/live/mkaua/stream/4.m3u8">ProServer-2</a></li>
+  <li><a href="./server.html?url=http://tv2.efiberbd.net:8080/live/mkaua/stream/3.m3u8">ProServer-3</a></li>
+  <li><a href="./server.html?url=https://mprod-cdn.toffeelive.com/toffee/fifa-paid/match-1/master_1000.m3u8">ProServer-4</a></li>
+  <li><a href="./server.html?url=https://d3awnlgqz0szay.cloudfront.net/out/v1/b841c366cbe540e6a106c3ba83e5c8d6/index.m3u8">ProServer-5</a></li>
+  <li><a href="./server.html?url=http://190.210.221.119/Processor3_9_chunklist_b1300000.m3u8">ProServer-6</a></li>
+  <li><a href="./server.html?url=http://redlabmcdn.s.llnwi.net/nv02/ryowa7hd/index.m3u8">ProServer-7</a></li>
+  <li><a href="./server.html?url=https://rbmn-live.akamaized.net/hls/live/590964/BoRB-AT/master_3360.m3u8">ProServer-8</a></li>
+  <li><a href="./server.html?url=http://live.sportstv.com.tr/hls/low/sportstv_fhd/index.m3u8">ProServer-9</a></li>
+  <li><a href="./server.html?url=http://217.174.225.146/hls/ch004_720/index.m3u8">ProServer-10</a></li>
 </ul>
 
 `;
